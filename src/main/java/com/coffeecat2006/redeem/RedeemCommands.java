8832c89f505e6bc22440ec5f93a95bc7830fe0a9
Coffeecat2006 <147393318+Coffeecat2006@users.noreply.github.com>
2025-04-22 20:40:29 +0800

    Corrected the parameter type in RedeemCommands and changed the string parameter to word to improve the accuracy of the command; adjusted the code format to enhance readability.

--- src/main/java/com/coffeecat2006/redeem/RedeemCommands.java
+++ src/main/java/com/coffeecat2006/redeem/RedeemCommands.java
@@ -1,4 +1,5 @@
 package com.coffeecat2006.redeem;
+
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import net.minecraft.server.command.CommandManager;
@@ -7,20 +8,24 @@ import net.minecraft.server.command.ServerCommandSource;
 public class RedeemCommands {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(CommandManager.literal("redeem")
-            .then(CommandManager.argument("code", StringArgumentType.string())
+            .then(CommandManager.argument("code", StringArgumentType.word())
                 .executes(ctx -> RedeemManager.redeem(
-                    ctx.getSource(), StringArgumentType.getString(ctx, "code")
+                    ctx.getSource(),
+                    StringArgumentType.getString(ctx, "code")
                 ))));
+
         dispatcher.register(CommandManager.literal("redeem_list")
             .requires(src -> src.hasPermissionLevel(2))
             .executes(ctx -> RedeemManager.list(ctx.getSource())));
+
+        // /redeem_add <code> "<text>" <limit> <time> <rules>
         dispatcher.register(CommandManager.literal("redeem_add")
             .requires(src -> src.hasPermissionLevel(2))
-            .then(CommandManager.argument("code", StringArgumentType.string())
-            .then(CommandManager.argument("text", StringArgumentType.greedyString())
-            .then(CommandManager.argument("limit", StringArgumentType.string())
-            .then(CommandManager.argument("time", StringArgumentType.string())
-            .then(CommandManager.argument("rules", StringArgumentType.string())
+            .then(CommandManager.argument("code", StringArgumentType.word())
+            .then(CommandManager.argument("text", StringArgumentType.string())
+            .then(CommandManager.argument("limit", StringArgumentType.word())
+            .then(CommandManager.argument("time", StringArgumentType.word())
+            .then(CommandManager.argument("rules", StringArgumentType.word())
             .executes(ctx -> RedeemManager.add(
                 ctx.getSource(),
                 StringArgumentType.getString(ctx, "code"),
@@ -29,11 +34,13 @@ public class RedeemCommands {
                 StringArgumentType.getString(ctx, "time"),
                 StringArgumentType.getString(ctx, "rules")
             ))))))));
+
         dispatcher.register(CommandManager.literal("redeem_remove")
             .requires(src -> src.hasPermissionLevel(2))
-            .then(CommandManager.argument("code", StringArgumentType.string())
+            .then(CommandManager.argument("code", StringArgumentType.word())
                 .executes(ctx -> RedeemManager.remove(
-                    ctx.getSource(), StringArgumentType.getString(ctx, "code")
+                    ctx.getSource(),
+                    StringArgumentType.getString(ctx, "code")
                 ))));
     }
 }
