226af60822d65194e9b96d4bc2965703740d0095
Coffeecat2006 <147393318+Coffeecat2006@users.noreply.github.com>
2025-05-02 23:05:42 +0800

    Adjust the import statements of MailMod.java and rearrange them to match the coding style

--- src/main/java/com/coffeecat2006/mail/MailMod.java
+++ src/main/java/com/coffeecat2006/mail/MailMod.java
@@ -3,8 +3,10 @@ package com.coffeecat2006.mail;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.world.World;
+import net.minecraft.server.world.ServerWorld;
 
 public class MailMod implements ModInitializer {
     @Override
