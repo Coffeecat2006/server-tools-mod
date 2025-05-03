294145b0843d46670cd5833ee5bc83b8950fb133
Coffeecat2006 <147393318+Coffeecat2006@users.noreply.github.com>
2025-05-03 16:52:07 +0800

    fix

--- src/main/java/com/coffeecat2006/mail/MailCommands.java
+++ src/main/java/com/coffeecat2006/mail/MailCommands.java
@@ -25,8 +25,8 @@ public class MailCommands {
                 .then(CommandManager.literal("send")
                     .requires(src -> src.hasPermissionLevel(2))
                     .then(CommandManager.argument("player", StringArgumentType.word())
-                        .then(CommandManager.argument("title", StringArgumentType.String())
-                            .then(CommandManager.argument("content", StringArgumentType.String())
+                        .then(CommandManager.argument("title", StringArgumentType.string())
+                            .then(CommandManager.argument("content", StringArgumentType.string())
                                 .then(CommandManager.argument("item", BoolArgumentType.bool())
                                     .executes(ctx -> MailManager.send(
                                         ctx.getSource(),
