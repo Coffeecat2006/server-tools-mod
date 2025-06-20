055bfa2d84e7e86cb5d3c20b4b8a3a3482635120
Coffeecat2006 <147393318+Coffeecat2006@users.noreply.github.com>
2025-06-20 16:23:57 +0800

    Fixed InvisibleMod's onInitializeServer method signature and updated fabric.mod.json to correctly configure the server entry point

--- src/main/java/com/coffeecat2006/invisible/InvisibleMod.java
+++ src/main/java/com/coffeecat2006/invisible/InvisibleMod.java
@@ -2,13 +2,18 @@ package com.coffeecat2006.invisible;
 
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.MinecraftServer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 
 public class InvisibleMod implements DedicatedServerModInitializer {
-
     @Override
-    public void onInitializeServer() {
+    public void onInitializeServer(MinecraftServer server) {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             InvisibleCommands.register(dispatcher);
+        });
+
+        ServerLifecycleEvents.SERVER_STARTED.register(srv -> {
+            InvisibleManager.init();
         });
     }
 }
