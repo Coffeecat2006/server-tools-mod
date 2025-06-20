11bdc4b9e82e5cfcd9499a4921008af26f9d7ad0
Coffeecat2006 <147393318+Coffeecat2006@users.noreply.github.com>
2025-06-20 16:27:54 +0800

    fix bug

--- src/main/java/com/coffeecat2006/invisible/InvisibleMod.java
+++ src/main/java/com/coffeecat2006/invisible/InvisibleMod.java
@@ -2,18 +2,13 @@ package com.coffeecat2006.invisible;
 
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.minecraft.server.MinecraftServer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 
 public class InvisibleMod implements DedicatedServerModInitializer {
+
     @Override
-    public void onInitializeServer(MinecraftServer server) {
+    public void onInitializeServer() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             InvisibleCommands.register(dispatcher);
-        });
-
-        ServerLifecycleEvents.SERVER_STARTED.register(srv -> {
-            InvisibleManager.init();
         });
     }
 }
