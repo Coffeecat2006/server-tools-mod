b91a5a4ea7afc4d19ae29051588df8639026d21d
Coffeecat2006 <147393318+Coffeecat2006@users.noreply.github.com>
2025-06-20 16:42:04 +0800

    fix bug

--- src/main/java/com/coffeecat2006/invisible/InvisibleMod.java
+++ src/main/java/com/coffeecat2006/invisible/InvisibleMod.java
@@ -3,12 +3,12 @@ package com.coffeecat2006.invisible;
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
-public class InvisibleMod implements DedicatedServerModInitializer {
-
+public class InvisibleMod implements ModInitializer {
     @Override
-    public void onInitializeServer() {
+    public void onInitialize() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             InvisibleCommands.register(dispatcher);
         });
     }
 }
+
