e6f8a10114fd21bf45d2a9ab259b8053448a55cd
Coffeecat2006 <147393318+Coffeecat2006@users.noreply.github.com>
2025-06-20 16:10:15 +0800

    Added InvisibleMod category to implement invisible command functionality

---
+++ src/main/java/com/coffeecat2006/invisible/InvisibleMod.java
@@ -0,0 +1,14 @@
+package com.coffeecat2006.invisible;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+
+public class InvisibleMod implements DedicatedServerModInitializer {
+
+    @Override
+    public void onInitializeServer() {
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            InvisibleCommands.register(dispatcher);
+        });
+    }
+}
